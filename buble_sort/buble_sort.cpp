3cc0b2282790c27b470fbd4d702991e2f43a23da
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-03-16 18:06:36 +0700

    tambahan void input

--- buble_sort/buble_sort.cpp
+++ buble_sort/buble_sort.cpp
@@ -2,6 +2,34 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int a[20];                  //deklarassi array a dengan ukuran 20
+int n;                      // Looping
+
+void input() {              //procedur untuk input
+    while (true) {          // Looping
+        cout << "Masukkan Banyaknya elemen pada array : ";        // out ke layar
+        cin >> n;           // inpit dari pengguna
+        if (n <= 20)        // jika n kurang dari atau sama dengan 20
+            break;          // keluar dari loop
+        else {              // jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // output ke layar
+        }
+
+    }
+    cout << endl;                                   // output baris kosong    
+    cout << "=====================" << endl;        // output ke layar
+    cout << "Masukkan Elemen Array" << endl;        // output ke layar
+    cout << "====================" << endl;         // output ke layar
+
+    for (int i = 0; i < n; i++) {                    // Looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";      // output ke layar
+        cin >> a[i];
+    }
+
+}
+
 
 int main()
 {
